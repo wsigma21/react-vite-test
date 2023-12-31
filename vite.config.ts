ee24ab08f7d1f2a8b3d149b568da552d73409828
wsigma21 <wallstlbl@gmail.com>
2023-12-31 10:55:15 +0900

    build: vite.config.tsからbaseを削除してみる

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? 'react-vite-test' : './',
   plugins: [react()],
 })
